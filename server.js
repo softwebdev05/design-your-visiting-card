6993680b06d8aff870dd0b292f0d8730bf30e4a5
softwebdev05 <softwebdev05@gmail.com>
2022-05-03 12:12:23 +0300

    created server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
+})
+
+app.listen(3000, () => {
+  console.log('listening on port 3000...')
+})
